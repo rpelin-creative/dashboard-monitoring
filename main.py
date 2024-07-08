4d964b719868ee34492baf4a8d361044d7e5162d
zeads <ruri.pelinandang@gmail.com>
2024-07-09 06:13:58 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Aplikasi Dashboard Monitoring
+"""
+import gempaterkini
+
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_data(result)
